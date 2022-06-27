9b84fd8e601824c12cdc8849c7f8d0f679297691
sinburi9 <107863534+sinburi9@users.noreply.github.com>
2022-06-27 19:36:20 +0900

    Add files via upload

---
+++ day04_00 (사칙연산).py
@@ -0,0 +1,22 @@
+num1 = 100  # =(이퀄) 이라는 할당 연산식
+num2 = 20   #  
+
+add = num1 + num2   # 덧셈
+print('add=', add)
+
+sub =num1 - num2    # 뺄셈
+print('sub =', sub)
+
+mu1 =num1 * num2     # 곱셈
+print('mu1 =', mu1)
+
+div1 = num1 / num2   # 나눗셈
+print('div1 = ',div1)
+
+div3 = num1 // 12 # 정수 나눗셈의 몱 계산
+print('div3 = ',div3)
+
+div2 = num1 % 7  # 나머지 계산
+print('div2 = 100 % 7 :',div2)
+square = num1**2    # 제곱연산
+print('square = ',square)
